d0fbf8d148885f6c5bef0b7975a34306c998a953
Ever David Osorio <everdavidosoriorivera@gmail.com>
2024-11-14 22:16:42 -0500

    Organizacion Principal

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -18,7 +18,7 @@ public class Hotel extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/Home.fxml"));
         
         Scene scene = new Scene(root);
         
@@ -26,9 +26,7 @@ public class Hotel extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
